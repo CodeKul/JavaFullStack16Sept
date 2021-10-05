8d61e03175859e9c4cf37cc4ce006580ed00e3ff
vaibhav <codekul.vaibhav@gmail.com>
2021-10-05 18:06:28 +0530

    Control  statements

---
+++ src/controlflowstatement/MultiplicationArray.java
@@ -0,0 +1,16 @@
+package controlflowstatement;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MultiplicationArray {
+
+    public static void main(String[] args) {
+
+        List<Integer> list1 = Arrays.asList(1, 2, 3, 4, 5);
+        List<Integer> list2 = Arrays.asList(5, 4, 3, 2, 1);
+
+
+
+    }
+}
