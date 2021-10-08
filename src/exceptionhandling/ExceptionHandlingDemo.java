9da88ec380fb110041737decbed27d1ad4e28e4e
vaibhav <codekul.vaibhav@gmail.com>
2021-10-08 17:45:57 +0530

    Exception Handling -> nested try, multiple catch and finally

--- src/exceptionhandling/ExceptionHandlingDemo.java
+++ src/exceptionhandling/ExceptionHandlingDemo.java
@@ -14,10 +14,30 @@ public class ExceptionHandlingDemo {
         System.out.println("Hello");
         System.out.println("Welcome");
         try {
+            try {
+                String str  = null;
+                System.out.println(str.length());
+            }catch (NullPointerException e){
+                System.out.println(e);
+            }
             j = i / 0;
-        } catch (ArithmeticException e) {
-            System.out.println(e);
+
+        } catch (NumberFormatException e) {
+
+            System.out.println("Number format Exc=>"+e);
+        } catch (ArithmeticException  e){
+            try {
+                int[] arr = {1,2,3,44};
+                System.out.println(arr[5]);
+            }catch (ArrayIndexOutOfBoundsException a){
+                System.out.println("Array index =>"+a);
+            }
+
+            System.out.println("Arithmetic Exc=>"+e);
+        } finally {
+            System.out.println("Finally block");
         }
+
         System.out.println("Hii");
         System.out.println("Hello");
         System.out.println("Welcome");
