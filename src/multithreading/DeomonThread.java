ada407ee584fa0f0d82823d69463c1875a7ccf31
codekulvaibhav <codekul.vaibhav@gmail.com>
2021-11-09 18:05:55 +0530

    Deomon Thread and Dead lock

---
+++ src/multithreading/DeomonThread.java
@@ -0,0 +1,17 @@
+package multithreading;
+
+public class DeomonThread extends Thread {
+
+    public static void main(String[] args) {
+        System.out.println(Thread.currentThread().getName());
+        DeomonThread t1 = new DeomonThread();
+        t1.setDaemon(true);
+        t1.start();
+        System.out.println(t1.isDaemon());
+    }
+
+    @Override
+    public void run() {
+        System.out.println("In Run  method");
+    }
+}
