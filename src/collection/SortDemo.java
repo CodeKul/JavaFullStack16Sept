65c40140c305ac3f53475a412590bc022504f64e
codekulvaibhav <codekul.vaibhav@gmail.com>
2021-11-17 17:54:09 +0530

    Generics and Comparable

---
+++ src/collection/SortDemo.java
@@ -0,0 +1,22 @@
+package collection;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class SortDemo {
+
+    public static void main(String[] args) {
+        List<Integer> integers = new ArrayList<>();
+        integers.add(104324);
+        integers.add(104);
+        integers.add(1430);
+        integers.add(140);
+
+        Collections.sort(integers,Collections.reverseOrder());
+
+        System.out.println(integers);
+
+    }
+
+}
